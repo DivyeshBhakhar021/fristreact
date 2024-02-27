ef2d1fb0a4c42b17fd0ca77d1a15f1ab582d9323
DivyeshBhakhar021 <divyeshbhakhar021@gmail.com>
2024-02-27 09:27:28 +0530

    27_2_2024_dricter

---
+++ src/componet/componet.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quam quibusdam blanditiis ducimus sed numquam dolorem deleniti aspernatur dolor expedita nostrum, eius earum voluptatibus totam voluptas, quo autem sint similique!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
