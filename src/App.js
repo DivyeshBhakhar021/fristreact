726f500c7a51cc5d9eca7be9cfb4db99950a8601
DivyeshBhakhar021 <divyeshbhakhar021@gmail.com>
2024-02-28 21:05:00 +0530

    28_2_2024_homework

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import  Component  from './componet/componet';
 
@@ -7,7 +7,4 @@ function App() {
     <Component></Component>
   );
 }
-
-
-
 export default App;
