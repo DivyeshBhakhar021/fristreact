8ca411a5bb9f3c49eec744c3b5322bd393869683
DivyeshBhakhar021 <divyeshbhakhar021@gmail.com>
2024-03-07 09:31:42 +0530

    7_3_2024_work

--- src/user/containers/product/Product.js
+++ src/user/containers/product/Product.js
@@ -5,6 +5,7 @@ function Product(props) {
 
     const [productData, setProduct] = useState([]);
     const [searchdata, setsearchdata] = useState('');
+    const [sort,setSort] = useState('');
 
     const getData = async () => {
         const resapons = await fetch("https://fakestoreapi.com/products");
@@ -16,25 +17,53 @@ function Product(props) {
         getData()
     }, []);
 
-    const handaldata = (v) => {
-        setsearchdata(v.target.value);
+
+    const handalfdata = () => {
+        let filteredProducts = productData.filter((p) =>
+            p.title.toLowerCase().includes(searchdata) ||
+            p.description.toLowerCase().includes(searchdata) ||
+            p.price.toString().includes(searchdata)
+
+        );
+
+        const sortdata = filteredProducts.sort((a, b) => {
+            if (sort === 'lh') {
+                return a.price - b.price
+            } else if (sort === 'hl') {
+                return b.price - a.price
+            } else if (sort === 'a') {
+                return a.title.localeCompare(b.title)
+            } else if (sort === 'z') {
+                return b.title.localeCompare(a.title)
+            }
+        });
+           
+        return filteredProducts
+
     }
 
-    const filteredProducts = productData.filter((p) =>
-        p.title.toLowerCase().includes(searchdata.toLowerCase())
-    );
-
+    const pdata = handalfdata()
 
     return (
         <div className='container'>
             <input type="text"
                 placeholder='Search by product'
                 value={searchdata}
-                onChange={handaldata} />
+                onChange={(event) => setsearchdata(event.target.value)}
+                className='form-control md-3 mt-3' />
+
+    <select class="form-select" aria-label="Default select example"  onChange={(event)=> setSort(event.target.value)}>
+        <option value="0">--Select</option>
+        <option value="lh">Product: low to high</option>
+        <option value="hl">Product: high to low</option>
+        <option value="a">Product: A - Z</option>
+        <option value="z">Product: Z - A</option>
+    </select>
+
             <div className='row'>
                 {
-                    filteredProducts.map((v) => (
-                        <div className='col-md-4 gy-4'>
+                    pdata.map((v) => (
+                        <div className='col-md-3 gy-4'>
                             <Card className="my-2 shadow-sm" color="primary" outline style={{ width: '18rem' }}>
                                 <img className='p-3' height={'250px'} alt="Sample" src={v.image} />
                                 <CardBody>
